449e0615ac8e6b673c5f62d0b9cd2f5c6868f993
owl <75400842+owl-birds@users.noreply.github.com>
2023-10-15 21:39:10 +0700

    inline event handler, event modifiers, component event(interesting)

---
+++ src/lib/events/Component_events.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  // Components can also dispatch events. To do so,
+  // they must create an event dispatcher. Update
+  // Inner.svelte:
+  import { createEventDispatcher } from "svelte";
+  const dispatch = createEventDispatcher();
+  //
+  const say_helo = () => {
+    dispatch("message", { text: "Hello!!!" });
+    // costum event handler
+  };
+</script>
+
+<button on:click={say_helo}>component event</button>
+
+<!-- COMPONENT EVENTS -->
+<!-- 
+NOTES
+
+createEventDispatcher must be called when the 
+component is first instantiated — you can't do it 
+later inside e.g. a setTimeout callback. This 
+links dispatch to the component instance.
+
+You can also try changing the event name to something 
+else. For instance, change dispatch('message', {...}) 
+to dispatch('greet', {...}) in Inner.svelte and change 
+the attribute name from on:message to on:greet in 
+App.svelte.
+-->
--- src/lib/events/Events.svelte
+++ src/lib/events/Events.svelte
@@ -8,20 +8,91 @@
     position.x = event.clientX;
     position.y = event.clientY;
   };
+
+  // COMPONENT EVENTS
+  import Component_events from "./Component_events.svelte";
+  const component_event_handler = (event: { detail: { text: string } }) => {
+    alert(event.detail.text + " Component Event");
+  };
 </script>
 
 <div on:pointermove={handle_move}>
   the pointer is at x:{position.x}, y:{position.y}
 </div>
 
+<!-- INLINE EVENT HANDLER -->
+<div
+  on:pointermove={(event) => {
+    position.x = event.clientX;
+    position.y = event.clientY;
+  }}
+>
+  inline event handler <br />
+  the pointer is at x:{position.x}, y:{position.y}
+</div>
+
+<!-- 
+  NOTES for inline event handler
+  In some frameworks you may see recommendations to 
+  avoid inline event handlers for performance reasons, 
+  particularly inside loops. That advice doesn't apply 
+  to Svelte — the compiler will always do the right 
+  thing, whichever form you choose.
+-->
+
+<!-- EVENT MODIFIERS -->
+<!-- 
+The full list of modifiers:
+
+- preventDefault — calls event.preventDefault() 
+    before running the handler. Useful for client-
+    side form handling, for example.
+- stopPropagation — calls event.stopPropagation(), 
+    preventing the event reaching the next element
+- passive — improves scrolling performance on 
+    touch/wheel events (Svelte will add it 
+    automatically where it's safe to do so)
+- nonpassive — explicitly set passive: false
+- capture — fires the handler during the capture 
+    phase instead of the bubbling phase
+- once — remove the handler after the first time it 
+    runs
+- self — only trigger handler if event.target is the 
+    element itself
+- trusted — only trigger handler if event.isTrusted 
+    is true, meaning the event was triggered by a 
+    user action rather than because some JavaScript 
+    called element.dispatchEvent(...)
+
+You can chain modifiers together, 
+e.g. on:click|once|capture={...}.
+-->
+<br />
+<button on:click|once={() => alert("BUTTON CLICKED")}>
+  Click me, u have only one chance
+</button>
+
+<!-- COMPONENT EVENTS -->
+<br />
+<Component_events on:message={component_event_handler} />
+
 <style>
-  div {
+  div:nth-child(1) {
     width: 100%;
     height: 100px;
     display: grid;
     justify-content: center;
     align-content: center;
     background-color: darkgoldenrod;
+    color: #fff;
+  }
+  div:nth-child(2) {
+    width: 100%;
+    height: 100px;
+    display: grid;
+    justify-content: center;
+    align-content: center;
+    background-color: darkslateblue;
     color: #fff;
   }
 </style>
